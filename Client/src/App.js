431aacce90fab985139a25765f6bb1fee8b4c316
NelsonCad <NelsonCad97@gmail.com>
2020-03-02 21:09:42 -0700

    added source page and logo to navbar, logo has too much empty space next to it

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'antd/dist/antd.css';
 import Dashboard from './pages/Dasboard';
+import Sources from './pages/sources';
 import Navbar from './components/Navbar';
 
 function App () {
     return (
         <Router>
             <Navbar />
-            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/sources" component={Sources} />
         </Router>
     );
 };
---
+++ Client/src/pages/sources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sources.css';
+
+function sources () {
+    return (
+        <div>
+            <p>I am ze swarces</p>
+        </div>
+    );
+};
+
+export default sources;
