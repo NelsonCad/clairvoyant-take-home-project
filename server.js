25ad4fea8b6d3bcf243d9c408d61dc3069bb518e
NelsonCad <NelsonCad97@gmail.com>
2020-03-06 01:00:30 -0700

    added server.js again

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+let chartInfo = require("./infoDataTonic");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(logger("dev"));
+
+//Routes
+
+//API routes start with [siteName]:PORT
+app.get("/api/info", function (req,res) {
+  return res.json(chartInfo)
+});
+//if no API routes are hit, send React app
+app.use(function (req,res) {
+  res.sendFile(path.join(__dirname, "../Client/build/index.html"));
+});
+
+// Serve up static assets (heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("Client/build"));
+  }
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
